d4201101da69d52e4d169f2ca848949470b90025
Nguyen Huy Duc <nguyenduc05021998@gmail.com>
2020-05-06 20:10:26 +0700

    seerbarsong

--- AppNgeNhac/app/src/main/java/com/example/appngenhac/MainActivity.java
+++ AppNgeNhac/app/src/main/java/com/example/appngenhac/MainActivity.java
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     public ImageView playbutton, nextbutton, previousbutton;
-    public TextView namesong;
+    public TextView namesong, currenttimesong, totaltimesong;
     public List<Song> listsong = new ArrayList<>();
     public MediaPlayer media;
     public int positionsong = 0;
+    public SeekBar seekbarsong;
+    public Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,14 @@ public class MainActivity extends AppCompatActivity {
         nextbutton = findViewById(R.id.ic_next);
         previousbutton = findViewById(R.id.ic_previous);
         namesong = findViewById(R.id.name_song);
+        currenttimesong = findViewById(R.id.current_time_song);
+        totaltimesong = findViewById(R.id.time_song);
+        seekbarsong = findViewById(R.id.seek_bar_song);
         createlistsong();
         changesong();
         media = MediaPlayer.create(MainActivity.this, listsong.get(positionsong).getFilemp3());
         media.start();
+        settotaltime();
         namesong.setText(listsong.get(positionsong).getNamesong());
 
         playbutton.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        eventupdateseekbar();
     }
 
     public void createlistsong() {
@@ -88,9 +99,44 @@ public class MainActivity extends AppCompatActivity {
     public void controlchangesong() {
         if (media.isPlaying()) {
             media.stop();
+            media = MediaPlayer.create(MainActivity.this, listsong.get(positionsong).getFilemp3());
+            media.start();
+            namesong.setText(listsong.get(positionsong).getNamesong());
+        } else if (!media.isPlaying()) {
+            media = MediaPlayer.create(MainActivity.this, listsong.get(positionsong).getFilemp3());
+            media.start();
+            namesong.setText(listsong.get(positionsong).getNamesong());
         }
-        media = MediaPlayer.create(MainActivity.this, listsong.get(positionsong).getFilemp3());
-        media.start();
-        namesong.setText(listsong.get(positionsong).getNamesong());
+        settotaltime();
+    }
+
+    public void settotaltime() {
+        int millisecondsSong = media.getDuration();
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millisecondsSong);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millisecondsSong) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisecondsSong));
+        String showtotaltime = String.valueOf(minutes) + ":" + String.valueOf(seconds);
+        totaltimesong.setText(showtotaltime);
+        seekbarsong.setMax(millisecondsSong);
+    }
+
+    public void eventupdateseekbar() {
+         mHandler = new Handler();
+        MainActivity.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                int millisecondsSong = 0;
+                if (media != null) {
+                    int mCurrentPosition = media.getCurrentPosition() / 1000;
+                    seekbarsong.setProgress(mCurrentPosition*1000);
+                    millisecondsSong = media.getCurrentPosition();
+                }
+                mHandler.postDelayed(this, 1000);
+                long minutes = TimeUnit.MILLISECONDS.toMinutes(millisecondsSong);
+                long seconds = TimeUnit.MILLISECONDS.toSeconds(millisecondsSong) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisecondsSong));
+                String showtotaltime = String.valueOf(minutes) + ":" + String.valueOf(seconds);
+                currenttimesong.setText(showtotaltime);
+            }
+        });
     }
 }
